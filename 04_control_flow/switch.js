0f6b1f916051b29c7d8d7c4e2bf4160ee6651ee0
Homeshu Harshit Bennett <89681873+Homeshubennet007@users.noreply.github.com>
2024-09-10 20:25:08 +0000

    coded

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,54 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const currentMonth = 9 
+
+switch (currentMonth) {
+    case 1:
+        console.log("january");
+        break;
+    case 2:
+        console.log("feburary");
+        break;
+    case 3:
+        console.log("March");
+        break;
+    case 4:
+        console.log("April");
+        break;
+    case 5:
+        console.log("May");
+        break;
+    case 6:
+        console.log("june");
+        break;
+    case 7:
+        console.log("july");
+        break;
+    case 8:
+        console.log("August");
+        break;
+    case 9:
+        console.log("September");
+        break;
+    case 10:
+        console.log("October");
+        break;
+    case 11:
+        console.log("November");
+        break;
+    case 12:
+        console.log("December");
+        break;
+
+    default:
+        console.log("Month not found");
+        
+        break;
+}
